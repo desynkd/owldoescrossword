9d70ce62b6deba080b6ae9bcfc3166b931338059
desynkd <ravindupeeris180@gmail.com>
2023-10-03 21:21:50 +0530

    getting input

--- puzzle.c
+++ puzzle.c
@@ -10,16 +10,30 @@ typedef struct Lines{
     //word can be fit horizontally in said line
 }Line;
 
+void createRow(Line* row, int size);
+void addRow(Line* row, char* input);
+
 int main()
 {
     Line* row = NULL;
+    int size=0;
     bool continueLoop = true;
     char* input;
 
-    while(continueLoop)
+    while(0)
     {
-        scanf("%c", input);
+        scanf("%s", input);
+        
         //take string as input and if its only a new line then stop?
+        if(input=="\n")
+        {
+            break;
+        }
+        else
+        {
+            createRow(row, ++size);
+            addRow(row+(size-1), input);
+        }
     }
 
     return 0;
@@ -35,4 +49,11 @@ void createRow(Line* row, int size)
     {
         row = (Line*)realloc(row, size*sizeof(Line));
     }
+}
+
+void addRow(Line* row, char* input)
+{
+    int strLen = strlen(input);
+    row->pos = (char*)malloc(sizeof(char)*strLen+1);
+    strcpy(row->pos, input); 
 }
