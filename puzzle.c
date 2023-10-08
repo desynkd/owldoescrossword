033372bcda012bf157a8ee9e880bfe1325c61c7e
desynkd <ravindupeeris180@gmail.com>
2023-10-09 00:31:01 +0530

    add initAval Function

--- puzzle.c
+++ puzzle.c
@@ -6,15 +6,22 @@
 
 typedef struct Lines{
     char* col;
-    //do i store the no of hashtags here to see if a
-    //word can be fit horizontally in said line
 }Line;
 
+typedef struct CrssWrdPlaces{
+    int row;
+    int col;
+    int aval;
+}CrssWrdPlace;
+
 void createRow(Line** row, int size);
+void createAvalBlank(CrssWrdPlace** avalBlank, int size);
 void addRow(Line* row, char* input);
+void initAval();
 
 Line* row = NULL;
-int size=0;
+CrssWrdPlace* avalBlank = NULL;
+int lineSize=0, blankSize=0;
 
 int main()
 {
@@ -32,19 +39,19 @@ int main()
         else
         {
             input[strcspn(input, "\n")] = 0;
-            createRow(&row, ++size);
-            addRow(row+(size-1), input);
+            createRow(&row, ++lineSize);
+            addRow(row+(lineSize-1), input);
         }
 
         #ifdef DEBUG1
-        for(n=0; n<size; n++)
+        for(n=0; n<lineSize; n++)
         {
             printf("DEBUG : n=%d | %s\n", n, row[n].col);
         }
         #endif
     }
 
-
+    initAval();
 
     return 0;
 }
@@ -61,6 +68,18 @@ void createRow(Line** row, int size)
     }
 }
 
+void createAvalBlank(CrssWrdPlace** avalBlank, int size)
+{
+    if (*avalBlank == NULL)
+    {
+        *avalBlank = (CrssWrdPlace*)malloc(size * sizeof(CrssWrdPlace));
+    }
+    else
+    {
+        *avalBlank = (CrssWrdPlace*)realloc(*avalBlank, size * sizeof(CrssWrdPlace));
+    }
+}
+
 void addRow(Line* row, char* input)
 {
     int strLen = strlen(input);
@@ -71,4 +90,59 @@ void addRow(Line* row, char* input)
 void initAval()
 {
     //calculate data of available spaces in struct
+    int size = blankSize;
+
+    for(int n=0; n<lineSize; n++)
+    {
+        for(int m=0; row[n].col[m] != '\0'; m++)
+        {
+            if(row[n].col[m]=='#')
+            {
+                createAvalBlank(&avalBlank,++size);
+                avalBlank[size-1].row = n;
+                avalBlank[size-1].col = m;
+
+                //identify pattern (N-E-S-W)
+                avalBlank[size-1].aval = 0;
+                //north
+                if(n > 0)
+                {
+                    if(row[n-1].col[m]=='#')
+                    {
+                        avalBlank[size-1].aval += 8;
+                    }
+                }
+                //east
+                if(row[n].col[m+1]=='#')
+                {
+                    avalBlank[size-1].aval += 4;
+                }
+                //south
+                if(n < lineSize-1)
+                {
+                    if(row[n+1].col[m]=='#')
+                    {
+                        avalBlank[size-1].aval += 2;
+                    }
+                }
+                //west
+                if(m > 0)
+                {
+                    if(row[n].col[m-1]=='#')
+                    {
+                        avalBlank[size-1].aval += 1;
+                    }
+                }
+            }
+        }
+    }
+    blankSize = size;
+
+    #ifdef DEBUG1
+    for(int n=0; n<size; n++)
+    {
+        printf("DEBUG : col=%d | row=%d | aval=%d\n", 
+        avalBlank[n].row, avalBlank[n].col, avalBlank[n].aval);
+    }
+    #endif
 }
